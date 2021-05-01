408a297533932d627b786ce8963aef487415b7cd
Jeffrey Hooker <jhookc@gmail.com>
2021-05-01 11:11:10 -0500

    core: scaffolding

---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class AppDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>React boilerplate</title>
+          <meta name="description" content="React SSR boilerplate"></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default AppDocument
